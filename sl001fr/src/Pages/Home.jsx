bc7404e62fc8715d405429d2ef4715ddb7b878ba
YAN TIANLI <e1539075@u.nus.edu>
2025-11-25 17:10:39 +0800

    git commit -m "SL001-13 [SL001_Frontend] Add NavBar into frontend structure"

--- sl001fr/src/Pages/Home.jsx
+++ sl001fr/src/Pages/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Top from "../Components/Top.jsx";
-
+import NavBar from "../Components/NavBar.jsx";
 // function top(){
 //     <Top></Top>
 // }
+function Sidebar(){
+
+}
+
+function Footer(){
+
+}
 
 export default function Home(){
     return (
         <div>
+            <NavBar/>
             <h1>Home Page</h1>
+            <Top></Top>
         </div>
     );
 }
--- sl001fr/src/main.jsx
+++ sl001fr/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App/App.css'                // 用你的 App.css
+import './scss/styles.scss'                // 用你的 App.css
 import { RouterProvider } from 'react-router-dom'
 import router from './App/router.jsx'     // src/App/router.jsx
 
