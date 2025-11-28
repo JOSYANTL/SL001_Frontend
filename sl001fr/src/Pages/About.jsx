3d30bc0b74df9768cc74fa307adf2034cd6f0e26
YAN TIANLI <e1539075@u.nus.edu>
2025-11-28 18:56:44 +0800

    ggit commit -m "SL001-3 [SL001_Frontend] Structure building Add about page in Structure"

---
+++ sl001fr/src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../scss/styles.scss'
+import NavBar from "../Components/NavBar.jsx";
+
+function About() {
+    return (
+        <div>
+            <NavBar/>
+            <h1>About</h1>
+        </div>
+    );
+}
+
+export default About;
